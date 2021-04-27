c74333b0f5dcc7aeab1a0348f1c5409d24f04168
Ethan Christensen <ethanlchristensen@outlook.com>
2021-04-27 05:23:49 -0500

    Added Restaurant Application and added 5 restaurants. Also added ability to choose restaurant when  making a reservation . . .

---
+++ main_project/reservation/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Reservation(models.Model):
+    reservationName = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+    peopleCount = models.IntegerField()
+    boothTable = models.TextField()
+    requests = models.TextField()
+    user = models.TextField()
+    restaurant = models.TextField()
+
