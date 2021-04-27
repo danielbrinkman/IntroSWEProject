819c6adcf2c21bb4a02614e654f793e6ff8c60db
tbauer5011 <tylerbauer@unomaha.edu>
2021-04-27 00:01:31 -0500

    I dunno anymore

---
+++ main_project/reservation/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from .models import Reservation
+from users.models import *
+
+
+def home(request):
+    context = {
+        'reservations': Reservation.objects.all()
+    }
+    return render(request, 'reservation/home.html', context)
+
+
+def about(request):
+    return render(request, 'reservation/about.html', {'title': 'About'})
+
+
+def make_reservation(request):
+    return render(request, 'reservation/make_reservation.html')
+
+
+def add_reservation(request):
+    x = request.POST['reservationName']
+    y = request.POST['peopleCount']
+    z = request.POST['tablePref']
+    requests = request.POST['comments']
+    date = request.POST['reservationTime']
+    new_item = Reservation(reservationName=x, peopleCount=y, boothTable=z, requests=requests, date=date)
+    new_item.save()
+    return redirect('reservation-home')
+
